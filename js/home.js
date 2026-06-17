44c423aba96feb291ac1f7cb0137a46045a1f272
kingarthur626-star <kingarthur626@gmail.com>
2026-06-17 21:01:45 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -4,10 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   if (!user) return;
 
   document.getElementById('homeContent').innerHTML = `
-    <p>登入成功</p>
-    <p>${escapeHtml(user.temple)}　${escapeHtml(user.name)}</p>
-    <p class="small-text">帳號：${escapeHtml(user.account)}</p>
-
     <div class="menu-area">
       <a class="menu-btn" href="annual.html">${escapeHtml(user.temple)} 今年道務</a>
     </div>
